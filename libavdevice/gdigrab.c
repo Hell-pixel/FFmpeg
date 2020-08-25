78565b0503d47eb02c583bfb1bd4d4270028bb61
Dmitry Kolobov <dimanolag@gmail.com>
2020-08-26 01:59:33 +0300

    add test 10

--- libavdevice/gdigrab.c
+++ libavdevice/gdigrab.c
@@ -145,7 +145,7 @@ gdigrab_region_wnd_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
  */
 static void DrawCircle(struct gdigrab *gdigrab, int pos_x, int pos_y){
 
-    HDC dest_dc = CreateCompatibleDC(gdigrab->dest_hdc);
+    //HDC dest_dc = CreateCompatibleDC(gdigrab->dest_hdc);
     //BitBlt(dest_dc, 0,0,1920,1080, gdigrab->dest_hdc, 0,0, SRCCOPY);
     //BitBlt(gdigrab->dest_hdc, 0,0,1920,1080,dest_dc , 0,0, SRCINVERT);
     //HBITMAP btmp = CreateCompatibleBitmap(gdigrab->dest_hdc, 1920, 1080);
@@ -169,15 +169,12 @@ static void DrawCircle(struct gdigrab *gdigrab, int pos_x, int pos_y){
     Ellipse(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, pos_x + radius, pos_y + radius);
     //AlphaBlend(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, diameter, diameter, dest_dc, pos_x - radius, pos_y - radius, diameter, diameter, bStruct);
     
-    if(GdiAlphaBlend(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, diameter, diameter, gdigrab->source_hdc, pos_x - radius, pos_y - radius, diameter, diameter, bStruct)){
+    if(GdiAlphaBlend(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, diameter, diameter, gdigrab->dest_hdc2, pos_x - radius, pos_y - radius, diameter, diameter, bStruct)){
         printf("\nSuccess\n");
     }else{
         printf("\nErr\n");
     }
-    //SelectObject(dest_dc, oldBmp);
     DeleteObject(brush);
-    DeleteObject(dest_dc);
-    //DeleteObject(btmp);
     DeleteObject(pen);
 }
 
