a188c5df9c4edb56f8828e6aba85fad7c61934c2
Dmitry Kolobov <dimanolag@gmail.com>
2020-08-25 23:57:54 +0300

    add test 8

--- libavdevice/gdigrab.c
+++ libavdevice/gdigrab.c
@@ -162,7 +162,9 @@ static void DrawCircle(struct gdigrab *gdigrab, int pos_x, int pos_y){
     bStruct.AlphaFormat = AC_SRC_ALPHA;
     
     Ellipse(dest_dc, pos_x - radius, pos_y - radius, pos_x + radius, pos_y + radius);
-    GdiAlphaBlend(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, diameter, diameter, dest_dc, pos_x - radius - gdigrab -> offset_x, pos_y - radius - gdigrab -> offset_y, diameter, diameter, bStruct);
+    if(GdiAlphaBlend(gdigrab->dest_hdc, pos_x - radius, pos_y - radius, diameter, diameter, dest_dc, pos_x - radius, pos_y - radius, diameter, diameter, bStruct)){
+        printf("\nSuccess\n");
+    }
     SelectObject(dest_dc, oldBmp);
     DeleteObject(brush);
     //DeleteObject(dest_dc);
